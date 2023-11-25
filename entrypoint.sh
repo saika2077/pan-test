38f30aef9686f96f7a7a9845d6364fea145e43d6
saika2077 <95666764+saika2077@users.noreply.github.com>
2023-11-25 17:59:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
-repo="alist-org/alist"
-tag=$(curl -s "https://api.github.com/repos/$repo/releases/latest" | grep -o '"tag_name": ".*"' | sed 's/"tag_name": "//;s/"//')
-download_url="https://github.com/$repo/releases/download/$tag/alist-linux-amd64.tar.gz"
-echo "${download_url}"
-wget "$download_url" -O temp.tar.gz
-rm -rf alist
-tar -xzvf temp.tar.gz && rm -f temp.tar.gz && chmod +x alist
-PASSWORD="${PASSWORD:-PASSWORD}"
-./alist admin set ${PASSWORD}
+#!/bin/bash
+
+repo="alist-org/alist" &&
+tag=$(curl -s "https://api.github.com/repos/$repo/releases/latest" | grep -o '"tag_name": ".*"' | sed 's/"tag_name": "//;s/"//') &&
+download_url="https://github.com/$repo/releases/download/$tag/alist-linux-amd64.tar.gz" &&
+echo "${download_url}" &&
+curl -LJO "$download_url" &&
+rm -rf alist &&
+tar -xzvf alist-linux-amd64.tar.gz && rm -f alist-linux-amd64.tar.gz && chmod +x alist &&
+./alist admin set PASSWORD &&
 ./alist server

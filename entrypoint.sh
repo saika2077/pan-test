392c88d99ba91032d5a732b7a44e65bbb006d775
saika2077 <95666764+saika2077@users.noreply.github.com>
2023-11-28 16:43:53 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 repo="alist-org/alist" &&
+
 tag=$(curl -s "https://api.github.com/repos/$repo/releases/latest" | grep -o '"tag_name": ".*"' | sed 's/"tag_name": "//;s/"//') &&
+
 download_url="https://github.com/$repo/releases/download/$tag/alist-linux-amd64.tar.gz" &&
-echo "${download_url}" &&
+
 curl -LJO "$download_url" &&
+
 rm -rf alist &&
+
 tar -xzvf alist-linux-amd64.tar.gz && rm -f alist-linux-amd64.tar.gz && chmod +x alist &&
-# ./alist admin set PASSWORD &&
+
+if [ ! -e data/config.json ] ; then
+
+  ./alist admin set PASSWORD
+
+fi
+
 ./alist server
